f549467e4b3b85ab1a72a3aa77ba7534d6263551
SangMinL96 <hwon3794@gmail.com>
2023-09-12 22:47:21 +0900

    ⚒️ Initial folder structure setting

---
+++ src/Calendar.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function App() {
+  return <div className="App">캘린더 테스트</div>;
+}
+
+export default App;
--- src/__test__/App.test.tsx
+++ src/__test__/App.test.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import App from '../Calendar';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/캘린더 테스트/i);
   expect(linkElement).toBeInTheDocument();
 });
