34dabba52abe928e28946280b370dbf08e821f4c
SangMinL96 <hwon3794@gmail.com>
2023-09-13 23:23:01 +0900

    📌 create week box

--- src/Calendar.tsx
+++ src/Calendar.tsx
@@ -1,7 +1,33 @@
 import React from "react";
 
 function App() {
-  return <div className="App">캘린더 테스트</div>;
+  return (
+    <div className="calendar-container">
+      <div className="calendar__week-box">
+        <div className="calendar-week">
+          <span>일</span>
+        </div>
+        <div className="calendar-week">
+          <span>월</span>
+        </div>
+        <div className="calendar-week">
+          <span>화</span>
+        </div>
+        <div className="calendar-week">
+          <span>수</span>
+        </div>
+        <div className="calendar-week">
+          <span>목</span>
+        </div>
+        <div className="calendar-week">
+          <span>금</span>
+        </div>
+        <div className="calendar-week">
+          <span>토</span>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
