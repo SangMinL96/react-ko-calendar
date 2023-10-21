24fec3cc2aa8d35b0e5a88b5c19ce722e12153df
SangMinL96 <hwon3794@gmail.com>
2023-10-21 12:28:28 +0900

    🚀 Date 형식 dayjs로 수정작업

--- src/components/Days.tsx
+++ src/components/Days.tsx
@@ -1,36 +1,33 @@
-import { DayType } from "../types";
+import { DayType } from '../types';
 
 type Props = {
-  days: DayType[];
-  handleDayClick: () => void;
+	days: DayType[];
+	handleDayClick: () => void;
 };
 
 function Days({ days }: Props) {
-  return (
-    <div className="calendar-container__day_view">
-      {days.map(({ day, isCurrentMonth, isWeekend, weekendType }, index) => {
-        let classNames;
-        if (!isCurrentMonth) {
-          classNames = `calendar-container__day_view__day disable`;
-        } else if (isWeekend) {
-          classNames = `calendar-container__day_view__day weekend_${
-            weekendType === "토"
-              ? "saturday"
-              : weekendType === "일"
-              ? "sunday"
-              : ""
-          }`;
-        } else {
-          classNames = "calendar-container__day_view__day";
-        }
-        return (
-          <button type="button" key={index} className={classNames}>
-            {day}
-          </button>
-        );
-      })}
-    </div>
-  );
+	console.log(days);
+	return (
+		<div className="calendar-container__day_view">
+			{days.map(({ day, isCurrentMonth, isWeekend, weekendType }, index) => {
+				let classNames;
+				if (!isCurrentMonth) {
+					classNames = `calendar-container__day_view__day disable`;
+				} else if (isWeekend) {
+					classNames = `calendar-container__day_view__day weekend_${
+						weekendType === '토' ? 'saturday' : weekendType === '일' ? 'sunday' : ''
+					}`;
+				} else {
+					classNames = 'calendar-container__day_view__day';
+				}
+				return (
+					<button type="button" key={index} className={classNames}>
+						{day}
+					</button>
+				);
+			})}
+		</div>
+	);
 }
 
 export default Days;
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,75 +1,76 @@
-import { DayType } from "../types";
+import dayjs from 'dayjs';
+import { DayType } from '../types';
 
-export const classes = (...args: (string | undefined)[]) =>
-  args.filter(Boolean).join(" ");
+export const classes = (...args: (string | undefined)[]) => args.filter(Boolean).join(' ');
 
 export const newDateKo = () => {
-  const now = new Date();
-  const utc = now.getTime() + now.getTimezoneOffset() * 60 * 1000;
-  const koreaTimeDiff = 9 * 60 * 60 * 1000;
-  const korNow = new Date(utc + koreaTimeDiff);
-  return korNow;
+	const now = new Date();
+	const utc = now.getTime() + now.getTimezoneOffset() * 60 * 1000;
+	const koreaTimeDiff = 9 * 60 * 60 * 1000;
+	const korNow = new Date(utc + koreaTimeDiff);
+	return korNow;
 };
 
 const onPrevMonthCheck = (month: number) => {
-  if (month === 0) {
-    return 12;
-  }
-  return month;
+	if (month === 0) {
+		return 12;
+	}
+	return month;
 };
 
 const onNextMonthCheck = (month: number) => {
-  if (month === 0) {
-    return 12;
-  }
-  return month;
+	if (month === 0) {
+		return 12;
+	}
+	return month;
 };
 
 export const createDays = (date: Date) => {
-  const days = [];
-  const year = date.getFullYear();
-  const month = date.getMonth();
-  const firstDay = new Date(year, month, 1);
-  const lastDay = new Date(year, month + 1, 0);
-  const prevMonthLastDate = new Date(year, month, 0).getDate();
-  const firstDayOfWeek = firstDay.getDay();
-  const lastDayOfWeek = lastDay.getDay();
+	const days = [];
+	const year = dayjs(date).year();
+	const month = dayjs(date).month() + 1;
+	const firstDay = dayjs(date).date(1); // 현재 월의 첫 번째 날을 나타내는 Date 객체를 생성합니다.
+	const lastDay = dayjs(date).endOf('month'); // 현재 월의 마지막 날을 나타내는 Date 객체를 생성합니다.
+	const prevMonthLastDate = dayjs(date).subtract(1, 'month').endOf('month').date(); // 이전 달의 마지막 날을 가져옵니다.
+	const firstDayOfWeek = dayjs(firstDay).day(); // 현재 월의 첫 번째 날의 요일을 가져옵니다.
+	const lastDayOfWeek = dayjs(lastDay).day(); // 현재 월의 마지막 날의 요일을 가져옵니다.
 
-  // 이전 달의 마지막 날짜를 추가
-  for (
-    let i = prevMonthLastDate - firstDayOfWeek + 1;
-    i <= prevMonthLastDate;
-    i++
-  ) {
-    days.push({
-      day: i,
-      isCurrentMonth: false,
-      goToClick: "back",
-      label: `${year}년 ${onPrevMonthCheck(month - 1)}월 ${i}일`,
-    });
-  }
+	const ddFormat = (n: number) => (n < 10 ? `0${n}` : n);
 
-  for (let i = 1; i <= lastDay.getDate(); i++) {
-    const is토요일 = (firstDayOfWeek + i) % 7 === 0;
-    const is일요일 = (firstDayOfWeek + i) % 7 === 1;
-    days.push({
-      day: i,
-      isCurrentMonth: true,
-      isWeekend: is토요일 || is일요일,
-      weekendType: is토요일 ? "토" : is일요일 ? "일" : "평일",
-      label: `${year}년 ${month}월 ${i}일`,
-    });
-  }
+	// 이전 달의 마지막 날짜를 추가
+	for (let i = prevMonthLastDate - firstDayOfWeek + 1; i <= prevMonthLastDate; i++) {
+		days.push({
+			day: i,
+			data: `${year}/${ddFormat(month - 1)}/${ddFormat(i)}`,
+			isCurrentMonth: false,
+			goToClick: 'back',
+			label: `${year}년 ${ddFormat(onPrevMonthCheck(month - 1))}월 ${ddFormat(i)}일`,
+		});
+	}
 
-  if (lastDayOfWeek !== 6) {
-    for (let i = 1; i <= 6 - lastDayOfWeek; i++) {
-      days.push({
-        day: i,
-        goToClick: "forward",
-        isCurrentMonth: false,
-        label: `${year}년 ${onNextMonthCheck(month + 1)}월 ${i}일`,
-      });
-    }
-  }
-  return days as DayType[];
+	for (let i = 1; i <= dayjs(lastDay).date(); i++) {
+		const is토요일 = (firstDayOfWeek + i) % 7 === 0;
+		const is일요일 = (firstDayOfWeek + i) % 7 === 1;
+		days.push({
+			day: i,
+			data: `${year}/${ddFormat(month)}/${ddFormat(i)}`,
+			isCurrentMonth: true,
+			isWeekend: is토요일 || is일요일,
+			weekendType: is토요일 ? '토' : is일요일 ? '일' : '평일',
+			label: `${year}년 ${ddFormat(month)}월 ${ddFormat(i)}일`,
+		});
+	}
+
+	if (lastDayOfWeek !== 6) {
+		for (let i = 1; i <= 6 - lastDayOfWeek; i++) {
+			days.push({
+				day: i,
+				data: `${year}/${ddFormat(month + 1)}/${ddFormat(i)}`,
+				goToClick: 'forward',
+				isCurrentMonth: false,
+				label: `${year}년 ${ddFormat(onNextMonthCheck(month + 1))}월 ${i}일`,
+			});
+		}
+	}
+	return days as DayType[];
 };
